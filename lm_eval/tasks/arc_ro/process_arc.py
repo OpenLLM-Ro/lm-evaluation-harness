b40a082bcbc12653038a95bd6687a779d3b7a6f5
Mihai Masala <mihaimasala@gmail.com>
2024-02-20 23:04:20 +0200

    Update foundational_with_options

--- lm_eval/tasks/arc_ro/process_arc.py
+++ lm_eval/tasks/arc_ro/process_arc.py
@@ -1,6 +1,6 @@
 import sys
 
-def doc_to_text_foundational_with_options(doc):
+def doc_to_text_chat_with_options(doc):
     choices = [doc["option_a"], doc["option_b"], doc["option_c"], doc["option_d"]]
     if doc["option_e"] != None:
         choices.append(doc["option_e"])
@@ -12,17 +12,17 @@ def doc_to_text_foundational_with_options(doc):
     string += "Răspuns: [/INST]"
     return string
 
-# [INST] <<SYS>>
-# Ești un asistent care răspunde la întrebări cu variante de răspuns. Alege răspunsul corect din variantele disponibile.
-# <</SYS>>
+def doc_to_text_foundational_with_options(doc):
+    choices = [doc["option_a"], doc["option_b"], doc["option_c"], doc["option_d"]]
+    if doc["option_e"] != None:
+        choices.append(doc["option_e"])
 
-# Întrebare: Rosaria a descoperit un tip de structură pe care nu a putut-o identifica într-o probă de apă dintr-un iaz. Profesoara ei s-a uitat în microscop și i-a explicat că erau zigote produse de unul dintre protiștii din proba ei. La care protist aparțin zigotii?
-# Variante:
-# - ameba
-# - euglena
-# - paramecium
-# - volvox
-# Răspuns: [/INST] volvox
+    string = "Întrebare: {0}\nVariante:\n".format(doc["instruction"])
+    for i, choice in enumerate(choices):
+        string += "- {0}\n".format(choice)
+    # string = string[:-1]
+    string += "Răspuns: "
+    return string
 
 
 def doc_to_text_chat(doc):
