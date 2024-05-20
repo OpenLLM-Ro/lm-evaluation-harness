442575b968ae27733148351125a88b849350ec3f
Alexandru Dima <alexdima98@gmail.com>
2024-05-20 12:16:16 +0300

    Finished laroseda integration

--- lm_eval/tasks/laroseda/utils.py
+++ lm_eval/tasks/laroseda/utils.py
@@ -1,7 +1,15 @@
 import datasets
 def process_docs(dataset: datasets.Dataset):
     def _helper(doc):
-        doc["rating"] = "positive" if doc["starRating"] > 3 else "negative" 
+        doc["rating"] = "pozitivă" if doc["starRating"] > 3 else "negativă" 
         return doc
 
     return dataset.map(_helper) # returns back a datasets.Dataset object
+
+def doc_to_target_bc(doc):
+    if doc["rating"] == "pozitivă":
+        return 1
+    return 0
+
+def doc_to_target_mc(doc):
+    return [1, 2, 4, 5].index(doc["starRating"])
