58245a8ecf8264ba815b9f215718bf48edc739fb
Alexandru Dima <alexdima98@gmail.com>
2024-05-20 14:00:46 +0300

    Added micro-f1 for laroseda

--- lm_eval/tasks/laroseda/utils.py
+++ lm_eval/tasks/laroseda/utils.py
@@ -1,4 +1,5 @@
 import datasets
+import evaluate
 def process_docs(dataset: datasets.Dataset):
     def _helper(doc):
         doc["rating"] = "pozitivă" if doc["starRating"] > 3 else "negativă" 
@@ -13,3 +14,11 @@ def doc_to_target_bc(doc):
 
 def doc_to_target_mc(doc):
     return [1, 2, 4, 5].index(doc["starRating"])
+
+def micro_f1_score(items):
+    f1_metric = evaluate.load("f1")
+    golds, preds = list(zip(*items))
+    f1_score = f1_metric.compute(references=golds, predictions=preds, average="micro")[
+        "f1"
+    ]
+    return f1_score
